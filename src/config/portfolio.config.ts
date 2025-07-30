eedab064c491bce10f3f04d8800a6c39c3d6d1bd
Shaahid <shaahid2453@gmail.com>
2025-07-30 11:38:21 -0700

    resume updated

--- src/config/portfolio.config.ts
+++ src/config/portfolio.config.ts
@@ -3,7 +3,7 @@ import type { Portfolio } from "@/types";
 export const portfolioConfig: Portfolio = {
   name: "shaah1d",
   tagline: "I build NextJS and Solidity applications",
-  resume: "https://drive.google.com/file/d/1BjTg5OaFc27VET-hu2RzQD_TWbsxIAF4/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1FskZGHa3l1_7D1qwq3bSRivUTQ62WY_F/view?usp=sharing",
   links: {
     github: "https://github.com/shaah1d",
     twitter: "https://x.com/shaah1d",
